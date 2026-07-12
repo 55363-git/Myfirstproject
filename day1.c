8d6d90f9139488fa47f3f22d433a00fb8adf0802
泪痕dy <lhdy.ldy@qq.com>
2026-07-12 20:33:22 +0800

    day1:1-100奇数和

--- day1.c
+++ day1.c
@@ -13,12 +13,20 @@ int main(void){
     else{
         printf("Both numbers are equal\n");
     }
-*/
+
     for(int i=1;i<10;i++){
         for(int j=1;j<=i;j++){
             printf("%d*%d=%d\t",j,i,i*j);
         }
         printf("\n");
     }
+*/
+    int sum=0;
+    for(int i=1;i<=100;i++){
+        if(i%2!=0){
+            sum+=i;
+        }
+    }
+    printf("Sum of odd numbers from 1 to 100 is: %d\n", sum);
     return 0;
 }
