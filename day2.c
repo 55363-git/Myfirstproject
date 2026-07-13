cf3aeb2928a36f15aa216087677d1093e3e82a74
泪痕dy <lhdy.ldy@qq.com>
2026-07-13 16:05:04 +0800

    day2:数组平均数

--- day2.c
+++ day2.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+/*
 int jj(){
     int sum=0;
     for(int j=1;j<=100;j++){
@@ -9,9 +9,26 @@ int jj(){
     }
     return sum;
 }
+*/
+
+float arr_average(int arr[], int size){
+    int sum=0;
+    for(int i=0;i<size;i++){
+        sum+=arr[i];
+    }
+    return (float)sum/size;
+}
 
 int main(){
-    int result=jj();
-    printf("The sum of odd numbers from 1 to 100 is: %d\n", result);
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    float average=arr_average(arr, n);
+    printf("The average of the array is: %.2f\n", average);
     return 0;
 }
