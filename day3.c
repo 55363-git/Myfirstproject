0739359d0bb449ad9f340b2f53cb1feeb9277bd5
泪痕dy <lhdy.ldy@qq.com>
2026-07-14 22:10:02 +0800

    day3:交换数字

--- day3.c
+++ day3.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
 int  main(void){
 /*
     int a;
@@ -11,11 +18,16 @@ int  main(void){
     printf("*p=%d\n", *p);
     *p=20;
     printf("a=%d\n", a);
-*/
     int a[5]={1,2,3,4,5};
     int *p=a;
     for(int i=0;i<5;i++){
         printf("a[%d]=%d\n",i,*(p+i));
     }
+*/
+    int a, b;
+    scanf("%d %d",&a,&b);
+    printf("Before swap: a=%d, b=%d\n",a,b);
+    swap(&a,&b);
+    printf("After swap: a=%d, b=%d\n",a,b);
     return 0;
 }
